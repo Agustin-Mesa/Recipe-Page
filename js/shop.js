113b40e3476740e36923064977728695794f8f26
Agustín <mesamauro1@gmail.com>
2020-08-25 23:32:24 -0300

    Modificaciones y se agregó el ABOUT ME
    1. Creación del about me.
    2. Arreglos del footer en index y recipes.
    3. Los menubar mobile son hechos en css y
    con eventlistener de js.

--- js/shop.js
+++ js/shop.js
@@ -1,15 +1,5 @@
 $(document).ready(function() {
 
-    //TOGGLE del MENU NAV
-    $('.menu-btn').on("click", function() {
-        $(".nav-ul").animate({
-            height: 'toggle',
-            'padding-top': 'toggle',
-            'padding-bottom': 'toggle',
-            opacity: 'toggle'
-        });
-    });
-
     //TOGGLE del FILTER
     $('#filter').on("click", function() {
         $(".main-filter-ul li ul").animate({
@@ -21,3 +11,6 @@ $(document).ready(function() {
     });
 });
 
+document.querySelector('.menu-btn').addEventListener('click', () => { 
+    document.querySelector('.nav-ul').classList.toggle('show');
+});
